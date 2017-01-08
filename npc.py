427583d6ab962cca406e794e160b0e97a8614b74
Jon Purdy <jon@jonpurdy.com>
2017-01-08 13:28:55 -0500

    Turned NPC into it's own function

--- npc.py
+++ npc.py
@@ -2,13 +2,27 @@
 
 import requests
 import xml.etree.ElementTree as etree 
+import sys
 
 # original curl command:
 # curl 'http://nikeid.nike.com/services/is_profane' -H 'Content-Type: application/x-www-form-urlencoded' --data 'appFlag=Nike_ID&levelCode=is_blocked&xml=%3Croot%3E%3Cword%3E'"hello"'%3C%2Fword%3E%3C%2Froot%3E'
 
 def main():
-	
+
+	# get the user's input for the word to be checked
 	word_to_be_checked = input("Enter the word to be checked: ")
+
+	# submits the word and returns back true or false
+	is_allowed = get_result_from_nike(word_to_be_checked)
+
+	# cleanly print the result
+	if is_allowed:
+		print("Nike considers the word '%s' to be clean." % word_to_be_checked)
+	else:
+		print("Nike considers the word '%s' to be profane." % word_to_be_checked)
+
+
+def get_result_from_nike(word_to_be_checked):
 
 	headers = {
 	    'Content-Type': 'application/x-www-form-urlencoded',
@@ -20,7 +34,12 @@ def main():
 	  'xml': '<root><word>%s</word></root>' % word_to_be_checked
 	}
 
-	result = requests.post('http://nikeid.nike.com/services/is_profane', headers=headers, data=data)
+	try:
+		print("Submitting request to Nike...")
+		result = requests.post('http://nikeid.nike.com/services/is_profane', headers=headers, data=data)
+	except Exception as e:
+		print(e)
+		sys.exit()
 
 	tree = etree.fromstring(result.text)
 
@@ -31,9 +50,11 @@ def main():
 					is_allowed = element.attrib[thing]
 
 	if is_allowed == "true":
-		print("Nike considers the word '%s' to be clean." % word_to_be_checked)
+		is_allowed = True
 	else:
-		print("Nike considers the word '%s' to be profane." % word_to_be_checked)
+		is_allowed = False
+
+	return is_allowed
 
 if __name__ == '__main__':
 	main()
